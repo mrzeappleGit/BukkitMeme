984a25500757b3027000ea54b078b06b1c698232
Matthew Thomas Stevens <matthew.stevens@matthewstevens.me>
2023-10-20 22:24:07 -0700

    Fixed detections

--- src/main/java/me/matthewstevens/meme/listeners/damageListener.java
+++ src/main/java/me/matthewstevens/meme/listeners/damageListener.java
@@ -1,11 +1,15 @@
 package me.matthewstevens.meme.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerDropItemEvent;
+
+import java.util.function.Supplier;
 
 public class damageListener implements Listener {
     int max = 100;
@@ -13,9 +17,17 @@ public class damageListener implements Listener {
     int range = max - min + 1;
     @EventHandler
     public void onPlayerDamage(EntityDamageEvent e){
+        if (!(e.getEntity().getType() == EntityType.PLAYER)) {
+            return; // It's not a player, so we just return
+        }
         int rand = (int)(Math.random() * range) + min;
-        if (rand > 20){
-            if(e.getClass().getName() == "PLAYER") {
+        Bukkit.getServer().getLogger().info(String.valueOf(rand));
+        if(rand > 50){
+            if(e.getCause().equals(DamageCause.FALL)){
+                Player hurtplayer = (Player) e.getEntity();
+                String hurtplayerString = hurtplayer.getDisplayName();
+                Bukkit.getServer().broadcastMessage("Oopsie woopsie " + hurtplayerString + " did a fally wally.");
+            } else {
                 Player hurtplayer = (Player) e.getEntity();
                 String hurtplayerString = hurtplayer.getDisplayName();
                 Bukkit.getServer().broadcastMessage(hurtplayerString + " needs some milk.");
--- src/main/java/me/matthewstevens/meme/listeners/swingSwordListener.java
+++ src/main/java/me/matthewstevens/meme/listeners/swingSwordListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
+import java.util.Objects;
+
 public class swingSwordListener implements Listener {
     int max = 100;
     int min = 1;
@@ -15,11 +17,11 @@ public class swingSwordListener implements Listener {
     @EventHandler
     public void onPlayerDamage(EntityDamageByEntityEvent e){
         int rand = (int)(Math.random() * range) + min;
-        if (rand > 20){
+        if (rand > 75){
             if (e.getDamager() instanceof Player){
                 Player player = (Player) e.getDamager();
-                if (player.getInventory().getItemInMainHand().equals(Material.DIAMOND_SWORD)) {
-                    Bukkit.getServer().broadcastMessage("<"+player.getPlayer().getDisplayName()+"> I can swing my sword, sword");
+                if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SWORD) {
+                    Bukkit.getServer().broadcastMessage("<"+ Objects.requireNonNull(player.getPlayer()).getDisplayName()+"> I can swing my sword, sword");
                     Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Meme.plugin, new Runnable() {
                         @Override
                         public void run() {
